ee0dc14955dda76a563c2ea9557be72a0d20e07c
ash-Tech07 <82700458+ash-Tech07@users.noreply.github.com>
2021-05-29 19:15:53 +0530

    Added form.php

---
+++ Form.php
@@ -0,0 +1,20 @@
+
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $to = "ashwin.anil8104@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $full_name = $_POST['fullname'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $full_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message. Thank You so much for giving us the feedback " . $full_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "<script>alert('Mail Sent. Thank you " . $full_name . ", we will contact you shortly.');</script>";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
